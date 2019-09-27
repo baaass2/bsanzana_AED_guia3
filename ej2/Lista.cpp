283ba16e91f563ed99344dc7f9b9b3df2a0af52f
baaass2 <jokerbass087@gmail.com>
2019-09-27 17:36:20 -0300

    se sube ej2

---
+++ ej2/Lista.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+#include "Lista.h"
+
+Lista::Lista() {}
+
+Nodo* Lista::crear (int num) {
+    Nodo *tmp;
+    Nodo *aux;
+    Nodo *aux2;
+    
+    tmp = new Nodo;
+    tmp->num = num;
+
+    if (this->raiz == NULL) { 
+        this->raiz = tmp;
+        tmp->sig = NULL;
+        this->ultimo = this->raiz;
+    }else {
+		aux = this->raiz;
+        while( aux != NULL ){
+			aux2 = aux->sig;
+			if(tmp->num <= aux->num){
+				tmp->sig = this->raiz;
+				this->raiz = tmp;
+				break;
+			}
+			else if(aux->num < tmp->num and aux2 == NULL){
+				aux->sig= tmp;
+				tmp->sig=NULL;
+				this->ultimo = tmp;
+				this->ultimo->sig = NULL;
+				break;	
+			}
+			else if(aux->num < tmp->num and aux2->num > tmp->num){
+				aux->sig = tmp;
+				tmp->sig = aux2;
+				break;
+			}else{
+				aux = aux->sig;
+			}
+		}
+    }
+    return this->raiz;
+}
+void Lista::imprimir () {  
+	Nodo *pivote = this->raiz;
+    while (pivote != NULL) {
+		cout << pivote->num<< endl;
+        
+        pivote = pivote->sig;
+    }
+}
+
+Nodo* Lista::getUltimo(){
+	return this->ultimo;
+}
+
---
+++ ej2/Programa.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+#include "Lista.h"
+
+void imprimirLista(Nodo* tmp){
+    while (tmp != NULL) {
+		cout << tmp->num << " - ";
+        
+        tmp = tmp->sig;
+    }
+    cout << "" << endl;
+}
+
+Nodo* ordenar(Nodo* pivote){
+	Nodo* q = pivote;
+	Nodo* actual = NULL;
+	int aux = 0;
+	
+	while (pivote != NULL){
+		actual = pivote->sig;
+		while(actual != NULL){
+			if(pivote->num > actual->num ){
+				aux = pivote->num;
+				pivote->num = actual->num;
+				actual->num = aux;
+			}
+			actual = actual->sig;
+		}
+		pivote = pivote->sig;
+	}
+	return q;
+}
+
+int main (void) {
+
+    Lista *lista = new Lista();
+    Lista *lista2 = new Lista();
+    
+    Nodo *pLast = NULL;
+    Nodo *p = NULL;
+    Nodo *q = NULL;
+    Nodo *t = NULL;
+    char stop = 'F';
+    int num = 0;
+    
+    cout << "Ingresando datos a lista 1..."<<endl;
+    while(stop != 'V'){
+		cout << "Ingrese entero a la lista: ";cin >> num;	
+		p=lista->crear(num);
+		pLast= lista->getUltimo();
+		cout << "Para detener escriba V, seguir F: ";cin >> stop;
+	}
+	stop='F';
+	
+    cout << "Ingresando datos a lista 2..."<<endl;
+    while(stop != 'V'){
+		cout << "Ingrese entero a la lista: ";cin >> num;	
+		q=lista2->crear(num);
+		cout << "Para detener escriba V, seguir F: ";cin >> stop;
+	}
+	
+	cout << "Lista 1..."<<endl;
+	imprimirLista(p);
+	cout << "Lista 2..."<<endl;
+	imprimirLista(q);
+	
+	pLast->sig = q;
+	t=p;
+	cout << "Lista 3 concatenada..."<<endl;
+	imprimirLista(t);
+	cout << "Lista 3 mezclada y ordenada..."<<endl;
+	t=ordenar(t);
+	imprimirLista(t);
+	
+    return 0;
+}
